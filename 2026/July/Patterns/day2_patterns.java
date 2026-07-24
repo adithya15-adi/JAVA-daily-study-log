0211fd3c799ef417a9787cb8337dfc1e5408e0e4
Adithya M <adithyaadi82948@gmail.com>
2026-07-24 22:19:09 +0530

    Add files via upload

---
+++ 2026/July/Patterns/day2_patterns.java
@@ -0,0 +1,145 @@
+/*
+public class day2_patterns {
+    public static void main(String[] args) {
+        int n = 5;
+        int i;
+        int j;
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for(j = 1; j <=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+ */
+
+// pattern no 2
+
+/*
+public class day2_patterns {
+    public static void main(String[] args) {
+        int n=5;
+        int i,j;
+        for(i=1;i<=n;i++){
+            for(j=1;j<=i-1;j++){
+                System.out.print(" ");
+            }
+            for(j=1;j<=n-i+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
+ */
+
+// pattern no 3
+
+/*
+public class day2_patterns {
+    public static void main(String[] args) {
+        int n=5;
+        int i,j;
+        for(i=1;i<=n;i++){
+            for(j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(j=1;j<=i+i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}  
+*/
+
+// pattern no 4
+
+/*
+public class day2_patterns {
+    public static void main(String[] args) {
+        int n=5;
+        int i,j;
+        for(i=1;i<=n;i++){
+            for(j=1;j<=i-1;j++){
+                System.out.print(" ");
+            }
+            for(j=1;j<=n-i+n-i+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
+ */
+
+// pattern no 5
+
+/*
+public class day2_patterns {
+    public static void main(String[] args) {
+        int n;
+        
+        n=5;
+        int i,j;
+        for(i=1;i<=n;i++){
+            for(j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+        n=4;
+        for(i=1;i<=n;i++){
+            for(j=1;j<=n-i+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+    
+}
+ */
+
+// pttern no 6
+
+public class day2_patterns {
+    public static void main(String[] args) {
+        int n;
+        
+        n=5;
+        int i,j;
+        for(i=1;i<=n;i++){
+            for(j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(j=1;j<=i+i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+        n=4;
+        for(i=1;i<=n;i++){
+            for(j=1;j<=i;j++){
+                System.out.print(" ");
+            }
+            for(j=1;j<=n-i+n-i+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+    
+}
