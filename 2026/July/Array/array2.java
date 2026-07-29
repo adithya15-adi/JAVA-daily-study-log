2ba24eb9f3f34062cf6b99157c0665009b110061
Adithya M <adithyaadi82948@gmail.com>
2026-07-29 23:43:15 +0530

    Add files via upload

---
+++ 2026/July/Array/array2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class array2 {
+    public static void main(String[] args) {
+        // ArrayList<Integer> b=new ArrayList<>();
+        // a.add(5);
+        // a.add(20);
+        // a.add(430);
+        // a.add(71);
+        // a.add(21);
+        // a.add(51);
+        // System.out.println(a);
+
+        // for (Integer i:a){
+        //     System.out.println(i);
+        // }
+
+        // int i;
+        // for(i=0;i<=a.size()-1;i++){
+        //     System.out.println(a.get(i));
+        // }
+        int[] a={10,20,30,40,50,60,70};
+        int[] psum=new int[a.length];
+        System.out.println(Arrays.toString(psum));
+    }
+}
